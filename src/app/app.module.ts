e82ae8afae20ea9bdff98887876ff8382d9308a8
Dharanidhar Reddy <yerramdharanidhar@gmail.com>
2024-03-16 09:50:21 -0700

    Added basic workflow-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { SearchComponent } from './search/search.component';
+import { FooterComponent } from './footer/footer.component';
+import {NgOptimizedImage} from "@angular/common";
+import { ResultComponent } from './search/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SearchComponent,
+    FooterComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
